e72ee0facbe494d8a526936cfaddf7e24cba9a8b
RJiazhen <jiazhen.ruan@outlook.com>
2025-07-16 14:46:07 +0800

    2025-07-16 14:46:05

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,13 @@ import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  dev: {
+    setupMiddlewares: (middlewares) => {
+      middlewares.push((req, res, next) => {
+        console.log('require.cache', require.cache)
+        const test = requrie('./test.ts')
+        next();
+      });
+    },
+  },
 });
