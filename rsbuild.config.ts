8e5ba2c065924955f9b1309886c3d228e57bba55
null <null>
2025-07-16 14:41:06 +0800

    2025-07-16 14:41:03

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,13 @@ import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  dev: {
+    setupMiddlewares: (middlewares) => {
+      middlewares.unshift((_, __, next) => {
+        console.log(require.cache)
+        const test = require('./test.ts')
+        next();
+      });
+    },
+  },
 });
