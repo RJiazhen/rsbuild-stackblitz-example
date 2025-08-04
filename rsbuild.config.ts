c227e75a100ca287e6375b3573a1e4abe355dc30
RJiazhen <jiazhen.ruan@outlook.com>
2025-08-04 14:56:29 +0800

    2025-08-04 14:56:28

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,22 @@
-import { defineConfig } from "@rsbuild/core";
-import { pluginReact } from "@rsbuild/plugin-react";
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  html: {
+    template: './public/index.html',
+  },
+  output: {
+    copy: [
+      {
+        from: './public',
+        globOptions: { ignore: ['**/index.html'] },
+      },
+    ],
+  },
+  server: {
+    publicDir: {
+      copyOnBuild: false,
+    },
+  },
 });
